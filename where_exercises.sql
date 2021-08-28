a46e9c330d6fbd8e6eb3ea84cd028c290515cbea
Anna Vu <anna.h.vu9@gmail.com>
2021-08-28 10:36:14 -0500

    remastered

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,9 +5,9 @@ use employees;
 select * from employees
 where first_name IN ('Irena', 'Vidya', 'Maya');
 
-/* I selected all from employees, told it to return all entries with those first names. */
+/* I selected all employees, told it to return all entries with those first names. 
+   Number of records returned was 709 */
 
--- 2.) Number of records returned was 709
 
 -- 3.) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. Enter a comment with the number of records returned. Does it match number of rows from Q2?
 
@@ -17,14 +17,7 @@ where first_name = 'Irena'
 or first_name = 'Vidya' 
 or first_name = 'Maya';
 
--- both yield the same, if you don't use in, you can use or but put them all in separate lines.
-
-use employees;
-select * from employees
-where first_name like 'Irena' 
-or first_name like 'Vidya' 
-or first_name like 'Maya';
--- 3.) Records returned was 709, so it does match with question 2
+ -- both yield the same of 709 results, if you don't use in, you can use or but put them all in separate lines. Records returned was 709, so it does match with question 2
 
 -- 4.) Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. Enter a comment with the number of records returned.
 select * from employees
@@ -46,13 +39,13 @@ where (first_name like 'Irena'
 
 -- 5.) Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E.
 
-
 select * from employees
 where last_name like 'E%';
 
 -- 5.) 7330 employees had a last name starting with 'E', the modulus indicates any character after.
 
--- 6.) Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. How many employees have a last name that ends with E, but does not start with E?
+-- 6.) Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. 
+-- How many employees have a last name that ends with E, but does not start with E?
 
 select * from employees
 where last_name like 'E%'
@@ -64,11 +57,15 @@ and last_name not like 'E%';
 
 -- 6.) 30723 employees have last names that start or end with an 'E',  and 23,393 employees have a last name that ends with an 'e' but does not start with an 'E'
 
--- 7.) Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees' last names end with E, regardless of whether they start with E?
+-- 7.) Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E.
+-- How many employees' last names end with E, regardless of whether they start with E?
+
+-- starts and ends with an E
 select * from employees
 where last_name like 'E%' 
 and last_name like '%E';
 
+-- ends with an E, regardless of whether it starts with an E
 select last_name from employees
 where last_name like '%E';
 
@@ -76,6 +73,8 @@ where last_name like '%E';
 
 -- 7.) 899 employees have a last name that starts with and ends with an 'E', and 24,292 employees have a last name that ends with an 'E'
 /* The modulus is a wildcard, and it basically says any character here or there until you define a letter or word */
+
+
 -- 8.) Find all current or previous employees hired in the 90s. Enter a comment with the number of employees returned.
 
 select * from employees
@@ -86,14 +85,14 @@ where hire_date between '1990-01-01' and '1999-12-31';
 select * from employees 
 where hire_date like '199%';
 
--- 8.) The number of employees hired in the 90s is 135,214
+-- The number of employees hired in the 90s is 135,214
 
 -- 9.) Find all current or previous employees born on Christmas. Enter a comment with the number of employees returned.
 
 select * from employees
 where birth_date like '%12-25';
 
--- 9.) the number of employees born on Christmas day is 842
+-- the number of employees born on Christmas day is 842
 
 -- 10.) Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the number of employees returned.
 select * from employees
@@ -106,20 +105,21 @@ select * from employees
 where hire_date like '199%' 
 and birth_date like '%12-25';
 
--- 10.) 362 employees were hired in the 90s and has a birthday on Christmas day
+-- 362 employees were hired in the 90s and has a birthday on Christmas day
+
 
 -- 11.) Find all current or previous employees with a 'q' in their last name. Enter a comment with the number of records returned.
 
 select * from employees
 where last_name like '%q%';
 
--- 11.) 1873 employees have a last name with the letter 'q' in it.
+-- 1873 employees have a last name with the letter 'q' in it.
 
 -- 12.) Find all current or previous employees with a 'q' in their last name but not 'qu'. How many employees are found?
 
 select * from employees where last_name like "%q%"
 and last_name not like "%qu%";
 
--- 12.) 547 employees have a 'q' in their last name, without 'qu' being in it. 
+-- 547 employees have a 'q' in their last name, without 'qu' being in it. 
 
 
